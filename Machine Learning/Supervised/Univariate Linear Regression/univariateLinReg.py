0c75c7d3dd2b880936b18511464cc5209806c31c
andhint <andrew.hintermeier@gmail.com>
2017-01-28 01:51:59 -0500

    Regression working
    Need to polish, comment and add plotting capabilities. Deleted iris
    dataset which isnt being used anymore

--- Machine Learning/Supervised/Univariate Linear Regression/univariateLinReg.py
+++ Machine Learning/Supervised/Univariate Linear Regression/univariateLinReg.py
@@ -10,13 +10,18 @@ X,y = np.split(data,2, axis=1)
 # add column of 1's before x values
 X = np.concatenate((np.ones((X.shape)),X), axis = 1)
 
-# plt.scatter(x,y)
-# plt.show()
 
-
-def gradientDescent(t0, t1, alpha, X, y):
+def gradientDescent(theta, alpha, X, y):
 	# one step of GD
-	return t0, t1
+	m = X.shape[0]
+
+	temp0 = theta[0][0] - alpha * (1.0 / m) * sum(np.dot(X,theta) - y)
+	temp1 = theta[1][0] - alpha * (1.0 / m) * sum((np.dot(X,theta) - y) * X[:,1].reshape(m,1))
+
+	theta[0][0] = temp0
+	theta[1][0] = temp1
+
+	return theta
 
 def costFunction(theta, X, y):
 	# computes cost function
@@ -25,42 +30,36 @@ def costFunction(theta, X, y):
 
 def main():
 	mincost = 5
-	maxIter = 1000
+	maxIter = 10000
 	alpha = 0.01
-	t0 = 0
-	t1 = 0
-	J = costFunction(t0, t1, X, y)
+	theta = np.array([[0],[0]],dtype='float32')
+	J = costFunction(theta, X, y)
 
-	t0History = [t0]
-	t1HIstory = [t1]
+	t0History = [theta[0][0]]
+	t1History = [theta[1][0]]
 	JHistory = [J]
 
 	for iterations in range(0,maxIter):
-		t0, t1 = gradientDescent(t0, t1, X, y)
-		J = costFunction(t0, t1, X, y)
+		theta = gradientDescent(theta, alpha, X, y)
+		J = costFunction(theta, X, y)
 
-		t0History.append(t0)
-		t1HIstory.append(t1)
+		t0History.append(theta[0][0])
+		t1History.append(theta[1][0])
 		JHistory.append(J)
-theta = np.array([[0],[0]])
-print theta
-print costFunction(theta, X, y)
 
-# xtest = np.array([[2],
-# 				  [3],
-# 				  [5]])
-# print np.concatenate((np.ones((xtest.shape)),xtest), axis = 1)
-
-# ytest = np.array([[2],
-# 				  [3],
-# 				  [5]])
-
-# theta = np.array([[2,3]])
-
-# # print xtest
-# # print
-# # print theta
+	
+	plt.plot(JHistory)
+	plt.show()
+	
+	plt.scatter(X[:,1],y, s=20, marker='.')
+	xx = np.arange(5,23)
+	yy = theta[0][0]+theta[1][0]*xx
+	plt.plot(xx,yy)
+	plt.show()
+	print(theta)
 
 
-# # print (1.0 / (2 * xtest.shape[0])) * sum(np.square(np.dot(xtest,theta.T) - ytest))
 
+
+
+main()
